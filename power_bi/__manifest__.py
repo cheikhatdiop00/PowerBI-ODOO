0c60538ac5d523d317714fde5dfcf95cf4a9f612
Cheikh Ahmed Tidiane Diop <78166974+cheikhatdiop00@users.noreply.github.com>
2022-02-14 17:57:06 +0000

    add power bi module

---
+++ power_bi/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Power BI Dashboard (Client Analysis)",
+    'version': '15.0.1',
+    'depends': ['base'],
+    'author': "Power Partners",
+    'company': "Power Partners",
+    'category': 'Invoicing',
+    'license': 'LGPL-3',
+    'website': 'https://www.powerpartners.pro/',
+    'images': ['static/description/cover.gif'],
+    'description': """Connect your Odoo Data easily to this premade Power BI dashboard.""",
+    'summary': """ This App allow to connect your Odoo invoicing data to a premade Power BI dashboard for Client Analysis.""",
+    'live_test_url': "https://app.powerbi.com/view?r=eyJrIjoiMGU3ZDIyY2QtYTA1NC00ODA4LWI4MWItMDA4NWFjOGJlYWNjIiwidCI6IjNhYmFkYThkLWY1ZmUtNGVkNS1hMDNlLWU4ZGY2NzJlOTQ3MiIsImMiOjl9&pageName=ReportSection",
+    'depends':['account'],
+    'support': 'pwr@powerpartners.pro',
+    'application': 'False',
+
+
+}
